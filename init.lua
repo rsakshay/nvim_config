a80f1d148db9c31c4812eb00218494d8345bb9d6
rsakshay <akshay.rachapudi@gmail.com>
2022-08-28 23:09:28 -0400

    Major code re-org. Moved all lua scripts into personal folder.

--- init.lua
+++ init.lua
@@ -7,34 +7,12 @@ vim.g.mapleader = ","
 vim.g.localleader = "\\"
 
 -- IMPORTS
-require('vars')      -- Variables
-require('opts')      -- Options
-require('keys')      -- Keymaps
-require('plug')      -- Plugins
+require('ak56.globals')      -- Global Variables
+require('ak56.options')      -- Options
+require('ak56.keymaps')      -- Keymaps
+require('ak56.plugins')      -- Plugins
 
---PLUGINS
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
-require('lualine').setup {
-    options = {
-        --theme = 'dracula-nvim'
-        theme = 'gruvbox'
-    }
-}
-require('nvim-autopairs').setup{}
-require('ak56/treesitter')
+-- PLUGINS
+require('ak56.treesitter')
+require('ak56.nvim-tree')
+require('ak56.autopairs')
