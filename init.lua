95ee02998dbe5724599cca82adf9774916baea7c
akshay-PC <akshay.rachapudi@gmail.com>
2022-08-28 21:04:11 -0400

    Updated nvim-tree config

--- init.lua
+++ init.lua
@@ -13,7 +13,23 @@ require('keys')      -- Keymaps
 require('plug')      -- Plugins
 
 --PLUGINS
-require('nvim-tree').setup{}
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
 require('lualine').setup {
     options = {
         --theme = 'dracula-nvim'
